4ab7fe16b64953ac2e1e1caa625ff200208165f6
1 <1@303-19>
2020-02-28 17:59:56 +0000

    Импорт, вывод ЖК, фильтр

--- jkh/Form1.Designer.cs
+++ jkh/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.jk_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.count_house = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.city = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.city_box = new System.Windows.Forms.ComboBox();
+            this.tolc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.jk_id,
+            this.name,
+            this.status,
+            this.count_house,
+            this.city});
+            this.dataGridView1.Location = new System.Drawing.Point(109, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 362);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // jk_id
+            // 
+            this.jk_id.HeaderText = "id ЖК";
+            this.jk_id.Name = "jk_id";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "название ЖК";
+            this.name.Name = "name";
+            // 
+            // status
+            // 
+            this.status.HeaderText = "статуса";
+            this.status.Name = "status";
+            // 
+            // count_house
+            // 
+            this.count_house.HeaderText = "Kоличество домов в ЖК";
+            this.count_house.Name = "count_house";
+            // 
+            // city
+            // 
+            this.city.HeaderText = "города";
+            this.city.Name = "city";
+            // 
+            // filter
+            // 
+            this.filter.Location = new System.Drawing.Point(322, 423);
+            this.filter.Name = "filter";
+            this.filter.Size = new System.Drawing.Size(134, 23);
+            this.filter.TabIndex = 1;
+            this.filter.Text = "Фильтр по городу";
+            this.filter.UseVisualStyleBackColor = true;
+            this.filter.Click += new System.EventHandler(this.filter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите город";
+            // 
+            // city_box
+            // 
+            this.city_box.FormattingEnabled = true;
+            this.city_box.Location = new System.Drawing.Point(195, 423);
+            this.city_box.Name = "city_box";
+            this.city_box.Size = new System.Drawing.Size(121, 21);
+            this.city_box.TabIndex = 4;
+            // 
+            // tolc
+            // 
+            this.tolc.Location = new System.Drawing.Point(521, 423);
+            this.tolc.Name = "tolc";
+            this.tolc.Size = new System.Drawing.Size(133, 23);
+            this.tolc.TabIndex = 5;
+            this.tolc.Text = "Перейти к ЖК";
+            this.tolc.UseVisualStyleBackColor = true;
+            this.tolc.Click += new System.EventHandler(this.tolc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 482);
+            this.Controls.Add(this.tolc);
+            this.Controls.Add(this.city_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filter);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(800, 521);
+            this.MinimumSize = new System.Drawing.Size(800, 521);
+            this.Name = "Form1";
+            this.Text = "Жилые комплексы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jk_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn count_house;
+        private System.Windows.Forms.DataGridViewTextBoxColumn city;
+        private System.Windows.Forms.Button filter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox city_box;
+        private System.Windows.Forms.Button tolc;
     }
 }
 
--- jkh/Form1.cs
+++ jkh/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace jkh
 {
+
     public partial class Form1 : Form
     {
+        public static DataGridViewRow current_row; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source = 303-19\\MSSQLSERVER2; Initial Catalog = jk; Integrated Security = true";
+            using (SqlConnection connection = new SqlConnection(@connectionString)) {
+                connection.Open();
+                SqlCommand com = new SqlCommand("SELECT TOP (1000) [jk_id], [name_lc], [build_status_lc], [house_count], [city] FROM[jk].[dbo].[live_complex]", connection);
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+                reader.Close();
+            }
+            using (SqlConnection connection = new SqlConnection(@connectionString))
+            {
+                connection.Open();
+                SqlCommand com = new SqlCommand("SELECT TOP (1000) [city] FROM[jk].[dbo].[live_complex]", connection);
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (!city_box.Items.Contains(reader[0]))
+                    {
+                        city_box.Items.Add(reader[0]);
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void filter_Click(object sender, EventArgs e)
+        {
+
+            String city = city_box.SelectedItem.ToString();
+            dataGridView1.Rows.Clear();
+            string connectionString = "Data Source = 303-19\\MSSQLSERVER2; Initial Catalog = jk; Integrated Security = true";
+            using (SqlConnection connection = new SqlConnection(@connectionString))
+            {
+                connection.Open();
+                SqlCommand com = new SqlCommand($"SELECT TOP (1000) [jk_id], [name_lc], [build_status_lc], [house_count], [city] FROM[jk].[dbo].[live_complex] WHERE [city] = '{city}'", connection);
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader[0], reader[1], reader[2], reader[3], reader[4]);
+                }
+                reader.Close();
+            }
+        }
+
+        private void tolc_Click(object sender, EventArgs e)
+        {
+            current_row = this.dataGridView1.CurrentRow;
+            LC_initerface i = new LC_initerface();
+            i.Show();
         }
     }
 }
